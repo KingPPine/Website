7f179cc772dc2651f0d75b1adeec3473d46202c9
KingPine <Kingp.pine@gmail.com>
2021-11-01 19:57:22 -0700

    adds writeups for each page

--- collapsible.js
+++ collapsible.js
@@ -5,11 +5,13 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    //console.log(content.style.maxHeight);
+    //console.log(content.style.maxHeight); <- used this to better understand why the if condition was not ""== 0"
     if (content.style.maxHeight) {
       content.style.maxHeight = null;
+      this.innerHTML = "Open Collapsible";
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
+      this.innerHTML = "Close Collapsible";
     }
   });
 } 
